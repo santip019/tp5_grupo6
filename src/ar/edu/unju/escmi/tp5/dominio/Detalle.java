88a8e69046eadb2ed03e3e1c29c47508d15a11fc
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-10-02 22:27:21 -0300

    Modificacion de las clases Detalle y Factura [Efimov Valentina]

--- src/ar/edu/unju/escmi/tp5/dominio/Detalle.java
+++ src/ar/edu/unju/escmi/tp5/dominio/Detalle.java
@@ -1,5 +1,79 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
 public class Detalle {
+    private byte cantidad;
+    private double precioUnitario;
+    private int codigoproducto;
+    private String descripcion;
+    private int descuento;
+
+    public Detalle() {
+    }
+
+    /*******/
+
+    public Detalle(byte cantidad, double precioUnitario, int codigoproducto, String descripcion, int descuento) {
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.codigoproducto = codigoproducto;
+        this.descripcion = descripcion;
+        this.descuento = descuento;
+    }
+
+    /******/
+
+    public byte getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(byte cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getCodigoproducto() {
+        return codigoproducto;
+    }
+
+    public void setCodigoproducto(int codigoproducto) {
+        this.codigoproducto = codigoproducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+     public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+
+    /*********/
+
+    public double aplicarDescuento () {
+        return (descuento * precioUnitario) / 100;
+    }
+
+    public double calcularImporteSinDescuento () {
+        return cantidad * precioUnitario;
+    }
+
+    public double calcularImporteConDescuento () {
+        return cantidad * aplicarDescuento();
+    }
 
 }
--- src/ar/edu/unju/escmi/tp5/dominio/Factura.java
+++ src/ar/edu/unju/escmi/tp5/dominio/Factura.java
@@ -1,5 +1,78 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
-public class Factura {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Factura {
+    private byte numeroFactura;
+    private int dni;
+    private LocalDate fecha;
+    private List<Detalle> detalles = new ArrayList<>();
+    private static byte contador = 1;
+
+    public Factura() {
+
+    }
+
+    public Factura(byte numeroFactura, int dni, LocalDate fecha) {
+        this.numeroFactura = contador++;
+        this.dni = dni;
+        this.fecha = fecha;
+    }
+
+
+    public byte getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(byte numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public List<Detalle> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(List<Detalle> detalles) {
+        this.detalles = detalles;
+    }
+
+    public void agregarDetalle(Detalle detalle) {
+        detalles.add(detalle);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Detalle d : detalles) {
+            total += d.calcularImporteConDescuento();
+        }
+        return total;
+    }
+
+    public void mostrarFactura() {
+        for (Detalle d : detalles) {
+            System.out.println("Producto: " + d.getDescripcion() +
+                               " Cantidad: " + d.getCantidad() +
+                               " Importe: " + d.calcularImporteConDescuento());
+        }
+        System.out.println("Total: " + calcularTotal());
+    }
 }
+
