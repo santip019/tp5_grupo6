f8365e4e9e2c7f775a0b0cf332e618859af3f630
Antonella Guadalupe Cruz <antoguadalupecruz@gmail.com>
2025-10-02 21:00:34 -0300

    Se agregan las collections de Factura, CLiente, Producto y Empleado [Cruz Antonella]

---
+++ src/ar/edu/unju/escmi/tp5/collections/CollectionCliente.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.escmi.tp5.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import ar.edu.unju.escmi.tp5.dominio.Cliente;
+import ar.edu.unju.escmi.tp5.dominio.ClienteMayorista;
+import ar.edu.unju.escmi.tp5.dominio.ClienteMinorista;
+
+public class CollectionCliente {
+    // La clave del map tiene que ser el DNI
+    public static Map<Integer, Cliente> clientes = new HashMap<>();
+
+    public static void guardarCliente(Cliente cliente) {
+        clientes.put(cliente.getDni(), cliente);
+    }
+
+    public static Cliente buscarCliente(int dni) {
+        return clientes.get(dni);
+    }
+
+    public static void precargarCliente() {
+        clientes.put(123, new ClienteMayorista(123, "Carlos", "Av. Siempre Viva", 1001));
+        clientes.put(456, new ClienteMinorista(456, "Ana", "Calle Belgrano", "PAMI"));
+    }
+}
