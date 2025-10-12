9548d8dc11e2b8f0e949cafd309be059ba1e1c31
Santi Padilla <smauriciop001@gmail.com>
2025-10-12 12:05:49 -0300

    Segunda version metodo realizarVenta corregido, aun no finalizado, altaProducto implementado, correccion constructor producto, cambio scanner para no cerrar y abrir en metodos de cada menu

--- src/ar/edu/unju/escmi/tp5/principal/MenuPrincipal.java
+++ src/ar/edu/unju/escmi/tp5/principal/MenuPrincipal.java
@@ -2,13 +2,19 @@ package ar.edu.unju.escmi.tp5.principal;
 
 import java.util.Scanner;
 
+import ar.edu.unju.escmi.tp5.collections.CollectionProducto;
+import ar.edu.unju.escmi.tp5.dominio.AgenteAdministrativo;
 import ar.edu.unju.escmi.tp5.dominio.EncargadoDeVentas;
+import ar.edu.unju.escmi.tp5.dominio.Factura;
+import ar.edu.unju.escmi.tp5.dominio.Producto;
 import ar.edu.unju.escmi.tp5.dominio.Cliente;
+import ar.edu.unju.escmi.tp5.dominio.ClienteMayorista;
+import ar.edu.unju.escmi.tp5.dominio.ClienteMinorista;
+import ar.edu.unju.escmi.tp5.dominio.Detalle;
 
 public class MenuPrincipal {
 
-    static void menuCliente() {
-        Scanner sc = new Scanner(System.in);
+    static void menuCliente(Scanner sc) {
         byte op;
         do {
             System.out.println("Menu de Clientes.");
@@ -34,11 +40,9 @@ public class MenuPrincipal {
             }
         } while (op != 2);
 
-        sc.close();
     }
 
-    static void menuEmpleado() {
-        Scanner sc = new Scanner(System.in);
+    static void menuEmpleado(Scanner sc) {
         byte op;
         byte op2;
         byte op3;
@@ -92,9 +96,8 @@ public class MenuPrincipal {
                     System.out.println("Menu Agente Administrativo");
                     do {
                         System.out.println("1 - Dar Alta de Producto");
-                        System.out.println("2 - Identificar Clientes");
-                        System.out.println("3 - Realizar Venta");
-                        System.out.println("4 - Salir");
+                        System.out.println("2 - Realizar Venta");
+                        System.out.println("3 - Salir");
                         System.out.print("Ingrese una opcion: ");
                         op3 = sc.nextByte();
                         sc.nextLine(); // Para limpiar el buffer del scanner
@@ -102,20 +105,99 @@ public class MenuPrincipal {
                         switch (op3) {
                             case 1:
                                 System.out.println("Dar Alta de Producto");
+                                System.out.print("Ingrese código de producto: ");
+                                int codigoProducto = sc.nextInt();
+                                sc.nextLine(); // Limpiar buffer
+
+                                System.out.print("Ingrese descripción: ");
+                                String descripcion = sc.nextLine();
+
+                                System.out.print("Ingrese precio unitario: ");
+                                double precioUnitarioNuevo = sc.nextDouble();
+
+                                int descuento;
+                                do {
+                                    System.out.print("Ingrese descuento (0, 25 o 30): ");
+                                    descuento = sc.nextInt();
+                                    if (descuento != 0 && descuento != 25 && descuento != 30) {
+                                        System.out.println("Valor inválido. Solo se permite 0, 25 o 30.");
+                                    }
+                                } while (descuento != 0 && descuento != 25 && descuento != 30);
+
+                                Producto productoNuevo = new Producto(codigoProducto, descripcion, precioUnitarioNuevo, descuento);
+
+                                AgenteAdministrativo.altaProducto(productoNuevo);
+
+                                System.out.println("Producto dado de alta correctamente.");
                                 break;
                             case 2:
-                                System.out.println("Identificar Clientes");
+                                System.out.println("Realizar Venta");
+                                // Paso 1: Identificar cliente
+
+                                Cliente cliente = AgenteAdministrativo.identificarCliente(); // Llama al metodo de Agente administrativo para identificar a que cliente se realiza la venta
+                                if (cliente == null) {
+                                    System.out.println("Cliente no encontrado. No se puede continuar con la venta.");
+                                    return;
+                                }
+
+                                // Paso 2: Crear factura
+                                Factura factura = new Factura(cliente);
+                                boolean seguirComprando = true;
+                                while (seguirComprando) {
+                                    // Paso 3: Selección de producto
+                                    System.out.print("Ingrese el código del producto: ");
+                                    int codigoProductoVenta = sc.nextInt();
+
+                                    Producto producto = CollectionProducto.buscarProducto(codigoProductoVenta);
+                                    if (producto == null) {
+                                        System.out.println("Producto no encontrado.");
+                                        continue;
+                                    }
+
+                                    System.out.print("Ingrese la cantidad: ");
+                                    int cantidad = sc.nextInt();
+                                    sc.nextLine(); // Para limpiar el buffer del scanner
+
+
+                                    if (producto.getStock() < cantidad) {
+                                        System.out.println("Stock insuficiente. Stock disponible: " + producto.getStock());
+                                    } else {
+                                        // Paso 4: Calcular precio según tipo de cliente
+                                        double precioUnitario = producto.getPrecio();
+
+                                        if (cliente instanceof ClienteMayorista) {
+                                            precioUnitario = precioUnitario / 2; // precio mayorista
+                                        } else if (cliente instanceof ClienteMinorista) {
+                                            ClienteMinorista cm = (ClienteMinorista) cliente;// Se convierte la referencia de tipo Cliente a tipo ClienteMinorista para usar todos los métodos y atributos de ClienteMinorista
+                                            precioUnitario = cm.aplicarDescuento(precioUnitario);// Verifica si el cliente minorista recibe descuento del 10%
+                                            }
+                                        }
+
+                                        // Crear detalle
+                                        Detalle detalle = new Detalle(producto, cantidad);
+                                        factura.agregarDetalle(detalle);
+
+                                        // Actualizar stock
+                                        producto.setStock(producto.getStock() - cantidad);
+
+                                        System.out.println("Detalle agregado: " + detalle);
+                                    }
+
+                                // Paso 5: Preguntar si sigue comprando
+                                System.out.print("¿Desea agregar otro producto? (s/n): ");
+                                String opcion = sc.next();
+                                seguirComprando = opcion.equalsIgnoreCase("s");
+                            
+                                // Paso 6: Guardar factura
+                                AgenteAdministrativo.realizarVenta(factura);
                                 break;
                             case 3:
-                                System.out.println("Realizar Venta");
-                                break;
-                            case 4:
                                 System.out.println("Saliendo del Menu Agente Administrativo...");
                                 break;
                             default:
                                 System.out.println("Opcion incorrecta. Intente nuevamente.");
                         }
-                    } while (op3 != 4);
+                    } while (op3 != 3);
                     break;
                 case 3:
                     System.out.println("Saliendo del Menu de Empleados...");
@@ -125,7 +207,6 @@ public class MenuPrincipal {
             }
         } while (op != 3);
 
-        sc.close();
     }
 
     public static void main(String[] args) {
@@ -144,10 +225,10 @@ public class MenuPrincipal {
 
             switch (op) {
                 case 1:
-                    menuCliente(); // Llama al menu de clientes
+                    menuCliente(sc); // Llama al menu de clientes
                     break;
                 case 2:
-                    menuEmpleado(); // Llama al menu de empleados
+                    menuEmpleado(sc); // Llama al menu de empleados
                     break;
                 case 3:
                     System.out.println("Saliendo del programa...");
